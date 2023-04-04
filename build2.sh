721f05789a412dbb185151537dc21b196ba0bf71
JustANormalTinker <105440921+JustANormalTinker@users.noreply.github.com>
2023-04-04 19:21:35 +0530

    [skip-ci]

--- build2.sh
+++ build2.sh
@@ -17,7 +17,7 @@ cd /tmp/rom # Depends on where source got synced
 tg_sendText "Lunching"
 # Normal build steps
 . build/envsetup.sh
-lunch lineage_a10-userdebug
+lunch lineage_whyred-userdebug
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
@@ -28,14 +28,14 @@ ccache -z
 tg_sendText "Starting Compilation.."
 
 # Compilation by parts if you get RAM issue but takes nore time!
-#mka api-stubs-docs -j8
-#mka system-api-stubs-docs -j8
-#mka test-api-stubs-docs -j8
-#mka bacon -j8 | tee build.txt
+mka api-stubs-docs -j8
+mka system-api-stubs-docs -j8
+mka test-api-stubs-docs -j8
+mka bacon -j8 | tee build.txt
 
-make bacon -j28 | tee build.txt
+#make bacon -j28 | tee build.txt
 
-(ccache -s && echo '' && free -h && echo '' && df -h && echo '' && ls -a out/target/product/a10/) | tee final_monitor.txt
+(ccache -s && echo '' && free -h && echo '' && df -h && echo '' && ls -a out/target/product/whyred/) | tee final_monitor.txt
 sleep 1s
 tg_sendFile "final_monitor.txt"
 sleep 2s
